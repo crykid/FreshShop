5b46c546d06d846f97e2866e374ad380481cdf7d
blank <blankmargin@hotmail.com>
2018-01-22 21:28:54 +0800

    1.导入商品数据； 2.配置资源图片访问路径

--- FreshShop/settings.py
+++ FreshShop/settings.py
@@ -131,3 +131,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# 配置图片资源访问路径
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- FreshShop/urls.py
+++ FreshShop/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from FreshShop.settings import MEDIA_ROOT
+from django.views.static import serve
 # from django.contrib import admin
 import xadmin
+
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
+    # 配置图片资源访问路径
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
 ]
