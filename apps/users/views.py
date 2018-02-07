88e6a366d891800b7f9038e529a5e777241ebc78
blank <blankmargin@hotmail.com>
2018-02-07 16:02:07 +0800

    登录注册完成

--- apps/users/views.py
+++ apps/users/views.py
@@ -12,6 +12,7 @@ from rest_framework.mixins import CreateModelMixin
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_jwt.serializers import jwt_encode_handler, jwt_payload_handler
 
 from .serializers import SmsSerializer, UserRegSerializer
 from utils.yunpian import YunPian
@@ -129,7 +130,24 @@ class SmsCodeViewset(CreateModelMixin, viewsets.GenericViewSet):
 
 class UserViewset(CreateModelMixin, viewsets.GenericViewSet):
     """
-    用户
+    用户注册
     """
     serializer_class = UserRegSerializer
     queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+
+        # 获取token
+        re_dict = serializer.data
+        payload = jwt_payload_handler(user)
+        re_dict["token"] = jwt_encode_handler(payload)
+        re_dict["name"] = user.name if user.name else user.username
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(re_dict, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        return serializer.save()
